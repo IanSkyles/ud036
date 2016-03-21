f7c5b704dbf1c464d235d495b17f071cb6555fcc
IanSkyles <ianskyles@gmail.com>
2016-03-21 16:40:12 -0700

    Added loop, print of current time

--- LessonOne_UseFunctions/Take_A_Break.py
+++ LessonOne_UseFunctions/Take_A_Break.py
@@ -1,2 +1,10 @@
 import webbrowser
-webbrowser.open("http://youtube.com/")
+import time
+
+breakCount = 0
+
+print("This program started at " + time.ctime())
+while breakCount < 3:
+    time.sleep(3)
+    webbrowser.open("http://youtube.com/")
+    breakCount = breakCount + 1
