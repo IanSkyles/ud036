6a51e660b1db986e5a7affdc37315a52133bd1f7
IanSkyles <ianskyles@gmail.com>
2016-03-21 19:16:31 -0700

    Added rename_files

--- LessonOne_UseFunctions/Take_A_Break.py
+++ LessonOne_UseFunctions/Take_A_Break.py
@@ -2,9 +2,10 @@ import webbrowser
 import time
 
 breakCount = 0
+totalBreaks = 3
 
 print("This program started at " + time.ctime())
-while breakCount < 3:
+while breakCount < totalBreaks:
     time.sleep(3)
     webbrowser.open("http://youtube.com/")
     breakCount = breakCount + 1
---
+++ LessonOne_UseFunctions/rename_files/rename_files.py
@@ -0,0 +1,20 @@
+
+import os
+
+def rename_files():
+    #get file names from a given folder
+    file_list = os.listdir(r"C:\Users\Ian Skyles\Documents\GitHub\ud036\LessonOne_UseFunctions\rename_files\prank")
+    print(file_list)
+
+    
+    #check current directory
+    #print(os.getcwd())
+
+    #change the current directory to the one with the pictures
+    os.chdir(r"C:\Users\Ian Skyles\Documents\GitHub\ud036\LessonOne_UseFunctions\rename_files\prank")
+    
+    #rename all of  the files/photos
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+
+rename_files()
