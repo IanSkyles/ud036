7265d9a37a667931dd418257dd302b7e000dc5e0
IanSkyles <ianskyles@gmail.com>
2016-03-22 04:02:36 -0700

    Added a profane text file

--- LessonTwo_UseClasses/PartThree_ProfanityEditor/check_profanity.py
+++ LessonTwo_UseClasses/PartThree_ProfanityEditor/check_profanity.py
@@ -1,10 +1,25 @@
 
-
+import urllib
 
 def read_text():
-    documentToCheck = open("C:\Users\Ian Skyles\Documents\GitHub\ud036\LessonTwo_UseClasses\PartThree_ProfanityEditor\movie_quotes.txt")
+    documentToCheck = open("C:\Users\Ian Skyles\Documents\GitHub\ud036\LessonTwo_UseClasses\PartThree_ProfanityEditor\movie_quotes_d.txt")
     contents_of_document = documentToCheck.read()
-    print(contents_of_document)
+    #print(contents_of_document)
     documentToCheck.close()
+    check_profanity(contents_of_document)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    #print(output)
+    connection.close()
+    if "true" in output:
+        print("Profanity altert!!!")
+    elif "false" in output:
+        print("This document has no curse words.")
+    else:
+        print("Could not scan the document properly")
+
 
 read_text()
+
